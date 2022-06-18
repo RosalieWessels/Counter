0c17c24dd9918d1deb9590b773efbc59f7e9cd77
RosalieWessels <rosalie.wessels@yahoo.com>
2022-06-18 10:18:18 -0700

    Initial Commit

---
+++ Shared/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 6/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
