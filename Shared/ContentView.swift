09c34789e184d546461de4c873432e72d4ed735f
RosalieWessels <rosalie.wessels@yahoo.com>
2022-06-18 11:55:22 -0700

    created a counting app

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var counter : Int = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(String(counter))
+                .font(Font.custom("AmericanTypewriter-Bold", size: 40))
+            HStack {
+                Button(action: {down()}) {
+                    Text("-")
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .background(Color.red)
+                .cornerRadius(20)
+                Button(action: {up()}) {
+                    Text("+")
+                        .foregroundColor(.white)
+                }
+                .padding()
+                .background(Color.green)
+                .cornerRadius(20)
+            }
+        }
+    }
+    
+    func up() {
+        counter += 1
+    }
+    
+    func down() {
+        counter -= 1
     }
 }
 
